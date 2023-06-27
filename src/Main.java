5f16ea89c05755936f087d8e9c963312bfbe5d53
ranjan66 <ranjan66@gmail.com>
2023-06-27 10:35:03 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Type your Name: ");
+        System.out.println("You wrote: " + sc.nextLine());
+        System.out.println("Type your age: ");
+        System.out.println(" your age: " + sc.nextInt());
+        sc.close();
+    }
+}
