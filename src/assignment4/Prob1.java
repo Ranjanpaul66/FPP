51edd5365ed0ba37ce005b978fa092abcd125ebf
ranjan66 <ranjan66@gmail.com>
2023-06-29 21:11:24 -0500

    4th assignment

--- src/assignment4/Prob1.java
+++ src/assignment4/Prob1.java
@@ -1,19 +1,31 @@
 package assignment4;
 
 public class Prob1 {
+    private static Prob1 instance=null;
+    private static Prob1 instance2=null;
+    private static Prob1 instance3=null;
+
     private Prob1(){
     }
 
 
     public static Prob1 getInstance1(){
-
-        return new Prob1();
+        if(instance==null){
+            instance = new Prob1();
+        }
+        return instance;
     }
     public static Prob1 getInstance2(){
-        return new Prob1();
+        if(instance2==null){
+            instance2 = new Prob1();
+        }
+        return instance2;
     }
     public static Prob1 getInstance3(){
-        return new Prob1();
+        if(instance3==null){
+            instance3 = new Prob1();
+        }
+        return instance3;
     }
 
     public static void main(String[] args) {
