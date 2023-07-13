aae09ee972c2e71e7f7d194e2f5b0ef217c467bc
ranjan66 <ranjan66@gmail.com>
2023-07-12 21:06:23 -0500

    8th assignment

---
+++ src/assignment8/prob2/Statistics.java
@@ -0,0 +1,14 @@
+package assignment8.prob2;
+import java.util.List;
+
+public class Statistics {
+    public static double computeSumOfSalaries(List<EmployeeData> aList) {
+        double totalSalaries = 0.0;
+        for (EmployeeData e : aList) {
+            totalSalaries += e.getSalary();
+        }
+        return totalSalaries;
+        //implement
+        //compute sum of all salaries of people in aList and return
+    }
+}
