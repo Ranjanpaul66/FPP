1c12d0486ad7cd303648a08ca647b9185931c00d
ranjan66 <ranjan66@gmail.com>
2023-07-19 20:48:43 -0500

    12th assignment

---
+++ src/assignment12/prob1/Main.java
@@ -0,0 +1,27 @@
+package assignment12.prob1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value;
+
+        System.out.print("Enter your number to validate: ");
+        try {
+            value = sc.nextInt();
+
+            if (value < 0 || value > 100) {
+                throw new UnsupportedOperationException("The inputs not in the range of 0-100");
+            }
+        } catch (InputMismatchException ex) {
+            throw new InputMismatchException("The wrong entry of data instead of int value.");
+        } catch (UnsupportedOperationException ex) {
+            System.out.println(ex.getMessage());
+            throw new UnsupportedOperationException(ex.getMessage());
+        }
+
+        System.out.println("Your number is valid.");
+    }
+}
---
+++ src/assignment12/prob2/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package assignment12.prob2;
+
+public class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
